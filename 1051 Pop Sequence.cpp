ffb8f85b74dd73414fecbf06298912da3fba9014
ErrorOverflow <wmlbuaa@163.com>
2019-07-04 18:56:57 +0800

    fuvk 1051

--- 1051 Pop Sequence.cpp
+++ 1051 Pop Sequence.cpp
@@ -6,57 +6,41 @@ int num[1010];
 int M,N,K;
 bool isNo=false;
 
-int judge(int low,int high){
-	int i,tmp,lowest,min=9999;
-	for(i=low;i<=high;i++){
-		if(num[i]<min){
-			lowest=i;
-			min=num[i];
-		}
-	}
-	if(lowest!=low){
-		if(lowest-high+1>M){
-			isNo=true;
-			return 0;
-		}
-		for(i=lowest-1,tmp=num[lowest];i>=low;i--){
-			if(num[i]!=--tmp){
-				isNo=true;
-				return 0;
-			}
-		}
-		judge(low,lowest);
-	}
-	if(lowest!=high){
-		/*
-		if(high-lowest>M){
-			cout<<"NO";
-			exit(0);
-		}
-		for(i=high-1,tmp=num[high];i>lowest;i--){
-			if(num[i]!=--tmp){
-				cout<<"NO";
-				exit(0);
-			}
-		}
-		*/
-		judge(lowest+1,high);
-	}
-}
-
 int main(){
-	int i,j;
+	int i,j,k,seq,pre;
 	cin>>M>>N>>K;
 	for(i=0;i<K;i++){
+		seq=1;
+		pre=0;
+		isNo=false;
+		bool isUsed[1010]={false};
 		for(j=0;j<N;j++){
 			cin>>num[j];
+			if(num[j]==pre-1){
+				seq++;
+				if(seq>M){
+					isNo=true;
+					//cout<<seq<<" ";
+				}
+			}else{
+				seq=1;
+			}
+			if(pre>num[j]){
+				for(k=num[j]+1;k<pre;k++){
+					if(!isUsed[k]){
+						isNo=true;
+						//cout<<"list ";
+						break;
+					}
+				}
+			}
+			pre=num[j];
+			isUsed[num[j]]=true;
 		}
-		judge(0,N-1);
 		if(isNo)
-			cout<<"NO";
+			cout<<"No"<<endl;
 		else
-			cout<<"YES";
-		isNo=false;
+			cout<<"YES"<<endl;
 	}
 	return 0;
 } 
